bf797cf302a9a359e4a2530e196e9573d71d9f88
Steve Reed <steven.m.reed@gmail.com>
2014-03-04 21:40:08 -0800

    Adds documentation to Dropship.java
    Adds javadoc to public class and public methods, as well as copyright.

--- src/main/java/dropship/Dropship.java
+++ src/main/java/dropship/Dropship.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 zulily, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dropship;
 
 import com.google.common.collect.Iterables;
@@ -11,8 +26,19 @@ import java.net.URLClassLoader;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * Dropship main class. This class contains the entry point when dropship is 
+ * run as an exacutable jar.
+ * 
+ */
 public final class Dropship {
 
+  /**
+   * Dropship command-line interface. Arguments specify an artifact by
+   * group ID, artifact ID, and an optional version. This specification is
+   * either explicit, using {@code group:artifact[:version]} syntax, 
+   * or implicit via an alias.
+   */
   public static void main(String[] args) throws Exception {
     ObjectGraph.create(new DropshipModule(args)).get(Dropship.class).run();
   }
